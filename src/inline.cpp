64cbee51336308aadf10b2552d47b3fadfcced56
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-12 17:45:32 +0100

    inline experience

--- src/inline.cpp
+++ src/inline.cpp
@@ -1,13 +1,13 @@
 #include "measure.h"
 
-const int MAXID = 2;
+const int MAXID = 3;
 using namespace std;
 
-void f(int i, int* x){
+void __attribute__ ((noinline)) f(int i, int* x){
   (*x) += i;
 }
 
-void exp(int id, int n, double* T, double* U){
+void exp(int id, int n){
   switch(id){
     case 0:{
       //a loop with two operation in the body
@@ -25,22 +25,27 @@ void exp(int id, int n, double* T, double* U){
         }
       }
       break;
+    case 2:{
+      int x=0;
+      int *p = &x;
+      for(int i=0; i<n; i++){
+        *p += i;
+      }
+      break;
+    }
     default:
       printf("invalid id\n");
   }
 }
 int main(){
-  const int n = 100000000;
-  const int repeat = 10;
-  //alloc
-  double* T = (double*)calloc(n, sizeof(double));
-  double* U = (double*)calloc(n, sizeof(double));
+  const int n = 1000000000;
+  const int repeat = 5;
   //record
   init(repeat, MAXID);
   for(int idRun = 0; idRun<=repeat; idRun++){
     for(int idCode=0; idCode<MAXID; idCode++){
       start();
-      exp(idCode, n, T, U);
+      exp(idCode, n);
       stop(idCode, idRun);
     }
   }

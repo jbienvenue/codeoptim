26eee4bb9fe8456b0ecd693a9a854e54679fe43c
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-12 11:13:06 +0100

    first programm

---
+++ src/fission.cpp
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "measure.h"
+#include <cstdio>
+
+#define MAXID 3
+using namespace std;
+
+void exp(int n, double* T, double* U, int id){
+    switch(id){
+        case 0:
+            for(int i=0; i<n; i++){
+                T[i]++;
+                U[i]++;
+            }
+            break;
+        case 1:
+            for(int i=0; i<n; i++){
+                T[i]++;
+            }
+            for(int i=0; i<n; i++){
+                U[i]++;
+            }
+            break;
+        case 2:
+            for(int i=n; i; i--){
+                T[i]++;
+            }
+            for(int i=n; i; i--){
+                U[i]++;
+            }
+            break;
+    }
+}
+int main(){
+    const int n=1000000;
+    const int repeat=50;
+    //alloc
+    double* T = (double*)calloc(n, sizeof(double));
+    double* U = (double*)calloc(n, sizeof(double));
+    //record
+    for(int id=0; id<MAXID; id++){
+        mesure tm_start=start();
+        for(int k=0; k<repeat; k++){
+            exp(n, T, U, id);
+        }
+        printf("%d\n", stop(tm_start));
+    }
+}

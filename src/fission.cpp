62681989232134536e32955baed1ea234d5faad7
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-12 13:40:46 +0100

    test fission and measure.h

--- src/fission.cpp
+++ src/fission.cpp
@@ -3,44 +3,52 @@
 const int MAXID = 3;
 using namespace std;
 
+
 void exp(int id, int n, double* T, double* U){
-    switch(id){
-        case 0:
-            for(int i=0; i<n; i++){
-                T[i]++;
-                U[i]++;
-            }
-            break;
-        case 1:
-            for(int i=0; i<n; i++){
-                T[i]++;
-            }
-            for(int i=0; i<n; i++){
-                U[i]++;
-            }
-            break;
-        case 2:
-            for(int i=n; i; i--){
-                T[i]++;
-            }
-            for(int i=n; i; i--){
-                U[i]++;
-            }
-            break;
-    }
+  switch(id){
+    case 0:
+      //a loop with two operation in the body
+      for(int i=0; i<n; i++){
+        T[i]++;
+        U[i]++;
+      }
+      break;
+    case 1:
+      //with loop fission
+      for(int i=0; i<n; i++){
+        T[i]++;
+      }
+      for(int i=0; i<n; i++){
+        U[i]++;
+      }
+      break;
+    case 2:
+      // traversal in reverse order
+      for(int i=n; i>=0; i--){
+        T[i]++;
+      }
+      for(int i=n; i>=0; i--){
+        U[i]++;
+      }
+      break;
+    default:
+      printf("invalid id\n");
+  }
 }
 int main(){
-    const int n=1000000;
-    const int repeat=50;
-    //alloc
-    double* T = (double*)calloc(n, sizeof(double));
-    double* U = (double*)calloc(n, sizeof(double));
-    //record
-    for(int id=0; id<MAXID; id++){
-        mesure tm_start = start();
-        for(int k = 0; k<repeat; k++){
-            exp(id, n, T, U);
-        }
-        printf("%d\n", stop(tm_start));
+  const int n = 100000000;
+  const int repeat = 2;
+  //alloc
+  double* T = (double*)calloc(n, sizeof(double));
+  double* U = (double*)calloc(n, sizeof(double));
+  //record
+  init(repeat, MAXID);
+  for(int idRun = 0; idRun<=repeat; idRun++){
+    for(int idCode=0; idCode<MAXID; idCode++){
+      start();
+      exp(idCode, n, T, U);
+      stop(idCode, idRun);
     }
+  }
+  report();
 }
--- src/measure.h
+++ src/measure.h
@@ -1,13 +1,43 @@
 #include <chrono>
 #include <cstdio>
 #include <cstdlib>
+
 typedef std::chrono::steady_clock::time_point mesure;
 
-mesure start(){
-    return std::chrono::steady_clock::now();
+mesure tm_start;
+int* exec_time_sum;//index by measure idCode
+int repeat;
+int nbIDCode;
+
+void start(){
+  tm_start=std::chrono::steady_clock::now();
 }
-int stop(mesure tm_start){
+void init(int repeatArg, int nbIDCodeArg){
+  nbIDCode = nbIDCodeArg;
+  repeat = repeatArg;
+  exec_time_sum = (int*)calloc(nbIDCode, sizeof(int));
+}
+
+void stop(int idCode, int idRun){
+  if(idRun != 0){
     mesure tm_end = std::chrono::steady_clock::now();
     int micro = std::chrono::duration_cast<std::chrono::microseconds>(tm_end-tm_start).count();
-    return micro/1000;
+    exec_time_sum[idCode] += micro;
+  }
+}
+
+void report(){
+  int baseline = exec_time_sum[0];
+  printf("for idCode = 0 : %dms\n", baseline/1000);
+  for(int idCode=1; idCode<nbIDCode; idCode++){
+    int st_time = exec_time_sum[idCode];
+    double r=(double)(baseline)/st_time;
+    printf("for idCode = %d : %dms ", idCode, st_time/1000);
+    if(r>2){
+      printf("(%.1fx)\n", r);
+    }else{
+      printf("(%s%.0f%%)\n", ((r<1)?"+":""), (1/r-1)*100);
+    }
+  }
+  free(exec_time_sum);
 }

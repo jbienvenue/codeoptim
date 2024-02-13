2ea8352552b0238b79e6c7b7adbde7566a0d6ebb
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-13 14:39:58 +0100

    compile with good args

---
+++ src/compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# usage: ./compile.sh src.cpp prog.exe
+# compile the source using either the compiler specified in a comment in the source file,
+# or using gcc -O3 by default une faute de frappe
+ARG=$1
+OUT=$2
+STR="//compile: "
+NBSTR=${#STR}
+RES=$(grep -P "^//compile: " $ARG | tail -c "+$STR")
+if [ "$RES" == "" ]; then
+  COM="gcc -O3"
+else
+  COM="$RES"
+fi
+$COM $ARG -o $OUT -lstdc++

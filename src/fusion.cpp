ff2701c780bd9d21987e4a9f53842c7915b1efb3
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-14 13:49:21 +0100

    better experiment with sum test for gcc execute the code

--- src/fusion.cpp
+++ src/fusion.cpp
@@ -2,43 +2,47 @@
 
 const int MAXID = 2;
 using namespace std;
+int total=0;
 
-
-void exp(int id, int n, double* T, double* U){
+void exp(int id, int n, double* T){
+  int s=0;
   switch(id){
-    case 0:
+    case 0:{
       //a loop with two operation in the body
       for(int i=0; i<n; i++){
         T[i] = i;
       }
       for(int i=0; i<n; i++){
-        U[i] = T[i]*i;
+        s += T[i];
+      }
       }
       break;
-    case 1:
-      for(int i=0; i<n; i++){
-        T[i] = i;
-        U[i] = T[i]*i;
+    case 1:{
+        for(int i=0; i<n; i++){
+          T[i] = i;
+          s += T[i];
+        }
+        break;
       }
-      break;
     default:
       printf("invalid id\n");
   }
+  total += s;
 }
 int main(){
   const int n = 100000000;
   const int repeat = 10;
   //alloc
   double* T = (double*)calloc(n, sizeof(double));
-  double* U = (double*)calloc(n, sizeof(double));
   //record
   init(repeat, MAXID);
   for(int idRun = 0; idRun<=repeat; idRun++){
     for(int idCode=0; idCode<MAXID; idCode++){
       start();
-      exp(idCode, n, T, U);
+      exp(idCode, n, T);
       stop(idCode, idRun);
     }
   }
   report();
+  printf("\tresult: %d\n", total);
 }

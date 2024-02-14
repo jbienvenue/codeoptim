ec7bb2118fc536f379ae35dd0de714463379be35
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-14 14:31:22 +0100

    with change of file and measure to common

--- src/wip/fission.cpp
+++ src/wip/fission.cpp
@@ -1,8 +1,7 @@
-#include "measure.h"
+#include "../common.h"
 
 const int MAXID = 2;
 using namespace std;
-const int L3Space = 4194304;
 
 void exp(int id, int n, double* T, double* U){
   switch(id){
@@ -32,14 +31,15 @@ void exp(int id, int n, double* T, double* U){
       printf("invalid id\n");
   }
 }
-int main(){
+int main(int argc, char* argv[]){
+  init(argc, argv);
   const int n = L3Space*3/(4*sizeof(double));
   const int repeat = 10;
   //alloc
   double* T = (double*)calloc(n, sizeof(double));
   double* U = (double*)calloc(n, sizeof(double));
   //record
-  init(repeat, MAXID);
+  init_tm(repeat, MAXID);
   for(int idRun = 0; idRun<=repeat; idRun++){
     for(int idCode=0; idCode<MAXID; idCode++){
       start();

2e1505f93b86f1328dfc7c242783c0515e9596d2
jbienvenue <joachim.bienvenue@passoire.fr>
2024-02-14 13:47:52 +0100

    try some tricks for make good expermient

--- src/wip/fission.cpp
+++ src/wip/fission.cpp
@@ -1,34 +1,31 @@
 #include "measure.h"
 
-const int MAXID = 3;
+const int MAXID = 2;
 using namespace std;
-
+const int L3Space = 4194304;
 
 void exp(int id, int n, double* T, double* U){
   switch(id){
     case 0:
       //a loop with two operation in the body
-      for(int i=0; i<n; i++){
-        T[i]++;
-        U[i]++;
+      for(int r=0; r<10; r++){
+        for(int i=0; i<n; i++){
+          T[i]++;
+          U[i]++;
+        }
       }
       break;
     case 1:
       //with loop fission
-      for(int i=0; i<n; i++){
-        T[i]++;
+      for(int r=0; r<10; r++){
+        for(int i=0; i<n; i++){
+          T[i]++;
+        }
       }
-      for(int i=0; i<n; i++){
-        U[i]++;
-      }
-      break;
-    case 2:
-      // traversal in reverse order
-      for(int i=n; i>=0; i--){
-        T[i]++;
-      }
-      for(int i=n; i>=0; i--){
-        U[i]++;
+      for(int r=0; r<10; r++){
+        for(int i=0; i<n; i++){
+          U[i]++;
+        }
       }
       break;
     default:
@@ -36,8 +33,8 @@ void exp(int id, int n, double* T, double* U){
   }
 }
 int main(){
-  const int n = 100000000;
-  const int repeat = 2;
+  const int n = L3Space*3/(4*sizeof(double));
+  const int repeat = 10;
   //alloc
   double* T = (double*)calloc(n, sizeof(double));
   double* U = (double*)calloc(n, sizeof(double));
